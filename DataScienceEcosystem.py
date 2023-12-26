bef1b5d5d34686203696d15180eeecb92bad28a6
Peterwens Dourolph Denival <109047885+Peterwens@users.noreply.github.com>
2023-12-25 21:27:43 -0500

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # In this notebook, Data Science Tools and Ecosystem are summarized. 
+# 
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# **Objectives**
+# -Introduction to Git and Github
+# -Introduction to  python
+# -Introduction to Jupiter notebook
+
+# Some of the popular languages that Data Scientists use are:
+# 1. Python
+# 2. R
+# 3. Sql
+
+# Some of the commonly used libraries used by Data Scientists include:
+# 1. Pandas
+# 2. Numpy
+# 3. Matplolib
+
+# | Data Science Tools|
+# | -------------|
+# |  Python|
+# | Github |
+# | Jupyter notebook |
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+
+# In[2]:
+
+
+#This a simple arithmetic expression to mutiply then add integers
+(3*4)+5
+
+
+# In[3]:
+
+
+#This will convert 200 minutes to hours by diving by 60
+200/60
+
+
+# ## Author
+# Peterwens dourolph Denival
